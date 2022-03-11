bff35a975ef4c99e88ce76e72db8dd20b8023aec
Riderpool1 <akshayashokan1054.com>
2022-03-11 16:46:48 +0530

    floating button

--- app/src/main/java/com/akshayashokcode/parkingspotfinder/presentation/MapScreen.kt
+++ app/src/main/java/com/akshayashokcode/parkingspotfinder/presentation/MapScreen.kt
@@ -1,10 +1,29 @@
 package com.akshayashokcode.parkingspotfinder.presentation
 
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ToggleOff
+import androidx.compose.material.icons.filled.ToggleOn
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 
 @Composable
 fun MapScreen(
     viewModel: MapsViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
 ) {
-    
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = if (viewModel.state.isFallOutMap) {
+                    Icons.Default.ToggleOff
+                }else Icons.Default.ToggleOn, contentDescription ="Toggle Fallout map")
+            }
+        }
+    ) {
+
+    }
 }
