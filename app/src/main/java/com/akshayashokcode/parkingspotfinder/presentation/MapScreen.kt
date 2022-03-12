1e3ea600c0c1c2a5ae80e1bdf7323701f8820ff3
Riderpool1 <akshayashokan1054.com>
2022-03-12 17:29:08 +0530

    google map

--- app/src/main/java/com/akshayashokcode/parkingspotfinder/presentation/MapScreen.kt
+++ app/src/main/java/com/akshayashokcode/parkingspotfinder/presentation/MapScreen.kt
@@ -1,5 +1,6 @@
 package com.akshayashokcode.parkingspotfinder.presentation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -8,12 +9,19 @@ import androidx.compose.material.icons.filled.ToggleOff
 import androidx.compose.material.icons.filled.ToggleOn
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
 
 @Composable
 fun MapScreen(
     viewModel: MapsViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
 ) {
     val scaffoldState = rememberScaffoldState()
+    val uiSettings=remember{
+        MapUiSettings(zoomControlsEnabled = false)
+    }
     Scaffold(
         scaffoldState = scaffoldState,
         floatingActionButton = {
@@ -24,6 +32,13 @@ fun MapScreen(
             }
         }
     ) {
+        GoogleMap(
+            modifier= Modifier.fillMaxSize(),
+            properties = viewModel.state.properties,
+            uiSettings = uiSettings,
+            onMapLongClick = {
 
+            }
+        )
     }
 }
